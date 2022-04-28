9fb4c99c397521a79168d5375bc8109cdbe23add
Daniel Hristov <danihristov1@abv.bg>
2022-04-28 20:08:06 +0300

    Parse Input

--- PrimeMultiplicationTable/Program.cs
+++ PrimeMultiplicationTable/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading;
 
 namespace PrimeMultiplicationTable
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = ParseInput();
+
+            var primeGenerator = new PrimeNumbersGenerator();
+            var primes = primeGenerator.GeneratePrimes(n);
+
+            var multiplicationTable = new PrimeMultiplicationTable();
+            multiplicationTable.GenerateMultiplicationTable(primes, n);
+        }
+
+        private static int ParseInput()
+        {   
+            Console.WriteLine("Input a number to find Prime numbers please...");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Generating HTML multiplication table with the first {n} numbers...");
+            Console.WriteLine();
+            Thread.Sleep(1000);
+            return n;
         }
     }
 }
