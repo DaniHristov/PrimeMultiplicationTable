f9d6ce59a89ef418a77775484ba6695b42f22304
Daniel Hristov <danihristov1@abv.bg>
2022-04-29 18:10:15 +0300

    Create Output.cs

---
+++ PrimeMultiplicationTable/Output.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace PrimeMultiplicationTable
+{
+    public class Output
+    {
+        static void Main(string[] args)
+        {
+            int n = ParseInput();
+
+            var primeGenerator = new PrimeNumbersGenerator();
+            var primes = primeGenerator.GeneratePrimes(n);
+
+            var multiplicationTable = new PrimeMultiplicationTable();
+            multiplicationTable.GenerateMultiplicationTable(primes, n);
+        }
+
+        private static int ParseInput()
+        {   
+            Console.WriteLine("Input a number bigger than 1 to find Prime numbers please...");
+            var n = int.Parse(Console.ReadLine());
+
+            while (n<2)
+            {
+                Console.WriteLine("Please input a number bigger than 1 ...");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Generating HTML multiplication table with the first {n} numbers...");
+            Console.WriteLine();
+            Thread.Sleep(1000);
+
+            return n;
+        }
+    }
+}
