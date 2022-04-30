a6971cdfc0ecd3953990f2fd426676c594fc3f38
Daniel Hristov <danihristov1@abv.bg>
2022-04-30 14:51:10 +0300

    Update Tests

--- PrimeMultiplicationTable.Tests/PrimeMultiplicationTableTests.cs
+++ PrimeMultiplicationTable.Tests/PrimeMultiplicationTableTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace PrimeMultiplicationTable.Tests
 {
@@ -42,6 +43,26 @@ namespace PrimeMultiplicationTable.Tests
             Assert.AreEqual(firstExpected, firstNumber);
             Assert.AreEqual(secondExpected, secondNumber);
             Assert.AreEqual(thirdExpected, thirdNumber);
+        }
+
+        [Test]
+        [TestCase(5)]
+        public void PrimeMultiplicationTableShouldReturnCorrectTable(int n)
+        {
+            var table = primeMultiplicationTable.GenerateMultiplicationTable(primeNumbersGenerator.GeneratePrimes(n),n);
+
+            int[,] expectedTable = new int[,]
+            {
+                { 0, 2, 3, 5, 7, 11 },
+                { 2, 4, 6, 10, 14, 22 },
+                { 3, 6, 9, 15, 21, 33 },
+                { 5, 10, 15, 25, 35, 55 },
+                { 7, 14, 21, 35, 49, 77 },
+                { 11, 22, 33, 55, 77, 121 },
+            };
+
+            Assert.AreEqual(expectedTable, table);
+
         }
     }
 }
